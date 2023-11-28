f5cfe1a78c63e85a6c8c4914dc16fa27445dfdf5
Hossein913 <Hossein913@gmail.com>
2023-11-28 19:15:57 +0330

    Debugt boothproduct enum

--- App.Domain.Core/_Products/Enums/BoothProductStatus.cs
+++ App.Domain.Core/_Products/Enums/BoothProductStatus.cs
@@ -8,7 +8,7 @@ namespace App.Domain.Core._Products.Enums
 {
     public enum BoothProductStatus
     {
-        Active = 0,
-        History = 1,
+        History = 0,
+        Active = 1,
     }
 }
