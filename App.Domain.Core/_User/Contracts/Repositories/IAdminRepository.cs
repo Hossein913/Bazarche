73e6ee535b4807a88c018e6c7150bb5f7686687b
Hossein913 <Hossein913@gmail.com>
2023-11-28 19:30:13 +0330

    debug AdminRepository :change Getdetails method

--- App.Domain.Core/_User/Contracts/Repositories/IAdminRepository.cs
+++ App.Domain.Core/_User/Contracts/Repositories/IAdminRepository.cs
@@ -5,8 +5,8 @@ namespace App.Domain.Core._User.Contracts.Repositories;
 public interface IAdminRepository
 {
     //Task<List<AdminOutputDto>> GetAll(CancellationToken cancellationToken);
-    Task<AdminOutputDto> GetDetail(int adminId, CancellationToken cancellationToken);
-    Task Update(AdminUpdateDto adminUpdate, CancellationToken cancellationToken);
+    Task<AdminOutputDto> GetDetail(CancellationToken cancellationToken);
+    Task Update(AdminUpdateDto adminUpdate, CancellationToken cancellationToken, bool saveChange = true);
 
     //Task Create(AdminCreateDto adminCreate, CancellationToken cancellationToken);
 
