49b682c1789f7aa212525ada8205437c395eaccf
Hossein913 <Hossein913@gmail.com>
2023-12-04 18:08:15 +0330

    [Develop].[Backend]:Add Hangfire to project

---
+++ App.Infra.Hangfire/HangfireServices.cs
@@ -0,0 +1,20 @@
+﻿using App.Domain.Core._Common.Contracts.Services;
+using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.Hangfire
+{
+    public class HangfireServices : IJobServices
+    {
+        public string AddNewJub<T>(Expression<Action<T>> expression, DateTimeOffset enqueueAt) {
+
+           return BackgroundJob.Schedule<T>(expression, enqueueAt);
+
+        }
+    }
+}
