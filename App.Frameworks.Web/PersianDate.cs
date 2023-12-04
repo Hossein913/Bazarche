fc39790173a0bd7493bfd3dcbf07ff3572387f2c
Hossein913 <Hossein913@gmail.com>
2023-12-04 22:47:40 +0330

    [Develop].[Backend].[FrameoWork]:Add Extention Mehtod to convert datetime to persian and vice versa

---
+++ App.Frameworks.Web/PersianDate.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace App.Frameworks.Web
+{
+    public static class PersianDate
+    {
+        public static string ToPersianDate(this DateTime dateTime)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            int year = persianCalendar.GetYear(dateTime);
+            int month = persianCalendar.GetMonth(dateTime);
+            int day = persianCalendar.GetDayOfMonth(dateTime);
+
+            return $"{year}/{month:00}/{day:00}";
+        }
+
+        public static DateTime ToGregorianDate(this PersianDateTime persianDate)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            DateTime gregorianDate = persianCalendar.ToDateTime(persianDate.year, persianDate.month, persianDate.day, persianDate.Hour, 0, 0, 0);
+            return gregorianDate;
+        }
+
+    }
+
+    public struct PersianDateTime
+    {
+        public short year { get; set; }
+        public short month { get; set; }
+        public short day { get; set; }
+        public short Hour { get; set; }
+    }
+}
