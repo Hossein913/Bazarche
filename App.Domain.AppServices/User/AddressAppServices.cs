a365e88ac956e9a719dee80859d2d33dcde7dfce
Hossein913 <Hossein913@gmail.com>
2023-12-09 18:02:18 +0330

    [Debug].[Backend].[Address]:Address and province appservices merged to gether

---
+++ App.Domain.AppServices/User/AddressAppServices.cs
@@ -0,0 +1,48 @@
+﻿using App.Domain.Core._User.Contracts.AppServices;
+using App.Domain.Core._User.Contracts.Services;
+using App.Domain.Core._User.Dtos.AddresseDtos;
+using App.Domain.Core._User.Dtos.ProvinceDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.AppServices.User
+{
+   public class AddressAppServices : IAddressAppServices
+    {
+        protected readonly IProvinceServices _provinceServices;
+
+       public AddressAppServices(IProvinceServices provinceServices)
+       {
+            _provinceServices = provinceServices;
+       }
+
+       public async Task<int> CreateProvince(ProvinceCreateDto provinceCreate, CancellationToken cancellationToken)
+       {
+            throw new NotImplementedException();
+       }
+
+       public async Task<AddressOutputDto> GetAddressDetail(int addressId, CancellationToken cancellationToken)
+       {
+            throw new NotImplementedException();
+       }
+
+       public async Task<List<ProvinceOutputDto>> GetAllProvinces(CancellationToken cancellationToken)
+       {
+            var result = await _provinceServices.GetAll(cancellationToken);
+            return result;
+       }
+
+       public async Task HardDeleteProvinces(int provinceId, CancellationToken cancellationToken)
+       {
+            throw new NotImplementedException();
+       }
+
+       public async Task UpdateProvinces(ProvinceUpdateDto provinceUpdate, CancellationToken cancellationToken)
+       {
+            throw new NotImplementedException();
+       }
+    }
+}
--- App.Domain.Core/_User/Contracts/AppServices/IAddressAppServices.cs
+++ App.Domain.Core/_User/Contracts/AppServices/IAddressAppServices.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using App.Domain.Core._User.Dtos.AddresseDtos;
+using App.Domain.Core._User.Dtos.ProvinceDto;
+using System;
 using System.Collections.Generic;
 
 namespace App.Domain.Core._User.Contracts.AppServices;
 
 public interface IAddressAppServices
 {
-
+    Task<AddressOutputDto> GetAddressDetail(int addressId, CancellationToken cancellationToken);
+    Task<List<ProvinceOutputDto>> GetAllProvinces(CancellationToken cancellationToken);
+    Task<int> CreateProvince(ProvinceCreateDto provinceCreate, CancellationToken cancellationToken);
+    Task HardDeleteProvinces(int provinceId, CancellationToken cancellationToken);
+    Task UpdateProvinces(ProvinceUpdateDto provinceUpdate, CancellationToken cancellationToken);
 }
