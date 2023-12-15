82f907196b34a9569a2db5c1dd438225e09cf243
Hossein913 <Hossein913@gmail.com>
2023-12-15 09:22:26 +0330

    [Develop].[Infrastructures].[DbContext:BazarcheContext]:An Overload of savechanges method added as comment to use for develop this method in future

--- App.Infra.Data.SqlServer.Ef/DbCntx/BazarcheContext.cs
+++ App.Infra.Data.SqlServer.Ef/DbCntx/BazarcheContext.cs
@@ -17,6 +17,7 @@ public class BazarcheContext : IdentityDbContext<AppUser, AppRole, int>
     public BazarcheContext(DbContextOptions<BazarcheContext> options) : base(options)
     {
     }
+
     #region DbSets
     public virtual DbSet<Address> Addresses { get; set; }
 
@@ -89,6 +90,29 @@ public class BazarcheContext : IdentityDbContext<AppUser, AppRole, int>
 
         base.OnModelCreating(modelBuilder);
     }
+
+
+    //public override int SaveChanges()
+    //{
+    //    var entries = ChangeTracker
+    //        .Entries()
+    //        .Where(e => e.Entity is BaseEntity && (
+    //                e.State == EntityState.Added
+    //                || e.State == EntityState.Modified));
+
+    //    foreach (var entityEntry in entries)
+    //    {
+    //        ((BaseEntity)entityEntry.Entity).UpdatedDate = DateTime.Now;
+
+    //        if (entityEntry.State == EntityState.Added)
+    //        {
+    //            ((BaseEntity)entityEntry.Entity).CreatedDate = DateTime.Now;
+    //        }
+    //    }
+
+    //    return base.SaveChanges();
+    //}
+
 
 }
 
